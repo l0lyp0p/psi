8a02706275e82eb0ad9a9b82a18b99ee64294fd0
l0lyp0p <64015762+l0lyp0p@users.noreply.github.com>
2025-04-04 14:57:50 +0200

    Update Lien.cs

--- PSISI/Lien.cs
+++ PSISI/Lien.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using PSISI2;
+using System;
+
+namespace PSISI2
+{
+    public class Lien<T>
+    {
+        public Noeud<T> De { get; }
+        public Noeud<T> Vers { get; }
+        public double Poids { get; }
+        public int LineId { get; } 
+
+        public Lien(Noeud<T> de, Noeud<T> vers, double poids, int lineId)
+        {
+            De = de;
+            Vers = vers;
+            Poids = poids;
+            LineId = lineId;
+        }
+    }
+}
