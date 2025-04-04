1d8fe59721e9e819490e3d50b5530e76d5d83f5f
l0lyp0p <64015762+l0lyp0p@users.noreply.github.com>
2025-04-04 15:51:05 +0200

    Update Lien.cs

--- PSISI/Lien.cs
+++ PSISI/Lien.cs
@@ -1,21 +1,15 @@
-﻿using PSISI2;
-using System;
-
-namespace PSISI2
+public class Lien<T>
 {
-    public class Lien<T>
-    {
-        public Noeud<T> De { get; }
-        public Noeud<T> Vers { get; }
-        public double Poids { get; }
-        public int LineId { get; } 
+    public Noeud<T> Depart { get; }
+    public Noeud<T> Arrivee { get; }
+    public double Poids { get; }
+    public string LineId { get; } 
 
-        public Lien(Noeud<T> de, Noeud<T> vers, double poids, int lineId)
-        {
-            De = de;
-            Vers = vers;
-            Poids = poids;
-            LineId = lineId;
-        }
+    public Lien(Noeud<T> depart, Noeud<T> arrivee, double poids, string lineId)
+    {
+        Depart = depart;
+        Arrivee = arrivee;
+        Poids = poids;
+        LineId = lineId;
     }
 }
