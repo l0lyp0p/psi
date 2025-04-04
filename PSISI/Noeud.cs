95d4d62d4314bc2d6d3e06c8adad1cd4ee00cbc4
l0lyp0p <64015762+l0lyp0p@users.noreply.github.com>
2025-04-04 14:58:10 +0200

    Update Noeud.cs

--- PSISI/Noeud.cs
+++ PSISI/Noeud.cs
@@ -1,1 +1,20 @@
+using System;
+
+namespace PSISI2
+{
+    /// <summary>
+    /// Classe générique représentant un nœud.
+    /// </summary>
+    public class Noeud<T>
+    {
+        public int Id { get; }
+        public T Value { get; }
+
+        public Noeud(int id, T value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
 ﻿
